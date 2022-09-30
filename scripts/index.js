cc81e1fd1a76534de15b90c369f407eddc75f115
Frosteel27 <frosteel27@yandex.ru>
2022-09-30 13:39:29 +0700

    feat: add upload popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,29 @@
-let popup = document.querySelector('.popup');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let editButton = document.querySelector('.profile__edit');
-let newName = document.querySelector('.popup__input[name=login]');
-let newJob = document.querySelector('.popup__input[name=job]');
-let formSubmit = document.querySelector('.popup__form');
-let closeButton = document.querySelector('.popup__close');
+const popup = document.querySelectorAll('.popup');
+const profilePopup = document.querySelector('.popup_type_profile');
+const uploadPopup = document.querySelector('.popup_type_upload');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const editButton = document.querySelector('.profile__edit');
+const newName = document.querySelector('.popup__input[name=login]');
+const newJob = document.querySelector('.popup__input[name=job]');
+const formSubmit = document.querySelector('.popup__form');
+const closeButton = document.querySelectorAll('.popup__close');
+const uploadButton = document.querySelector('.profile__add');
 
-function openPopup() {
-    popup.classList.remove('popup_hidden');
-    newName.value = profileName.textContent;
-    newJob.value = profileJob.textContent;
+function openPopup(evt) {
+    console.log(evt.target)
+    if (evt.currentTarget == editButton) {
+        profilePopup.classList.remove('popup_hidden');
+        newName.value = profileName.textContent;
+        newJob.value = profileJob.textContent;
+    }
+    if (evt.currentTarget == uploadButton) {
+        uploadPopup.classList.remove('popup_hidden');
+    }
 }
 
-function closePopup() {
-    popup.classList.add('popup_hidden')
+function closePopup(evt) {
+    evt.target.closest('.popup').classList.add('popup_hidden');
 }
 
 function savePopup(evt) {
@@ -25,5 +34,8 @@ function savePopup(evt) {
 }
 
 editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-formSubmit.addEventListener('submit', savePopup)
+formSubmit.addEventListener('submit', savePopup);
+uploadButton.addEventListener('click', openPopup);
+closeButton.forEach(x => {
+    x.addEventListener('click', closePopup)    
+});
